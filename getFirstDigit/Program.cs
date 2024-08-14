e27f7cf9f1ad15b836e66104ed6e6898b38f6997
Abyss363 <fortuneodo@gmail.com>
2024-08-14 11:08:06 +0100

    Add project files.

---
+++ getFirstDigit/Program.cs
@@ -0,0 +1,26 @@
+﻿try {
+    Console.Write("Input an integer: ");
+    string num = Console.ReadLine();
+    int numLength = num.Length;
+
+    int[] numArray = new int[numLength];
+
+    for (int i = 0; i < numLength; i++)
+    {
+        numArray[i] = Convert.ToInt32(num.Substring(i, 1));
+
+    }
+
+    foreach(int i in  numArray)
+    {
+        Console.Write(i);
+    }
+}
+catch
+{
+    Console.WriteLine("\nSomething Went Wrong");
+}
+finally
+{
+    Console.WriteLine("\nThank you for using our program");
+}
